5dfa0fce55295b3790c352c480a70b951a24ed40
Igor Marques <soigormarques@gmail.com>
2024-02-03 19:00:35 -0300

    feat: Desenvolvimento e estilização banner principal

--- wp-content/themes/adsplay/page-template/midia-programatica.php
+++ wp-content/themes/adsplay/page-template/midia-programatica.php
@@ -17,8 +17,54 @@ get_header();
             <span>Veja também</span>
             <a href="<?php echo get_site_url(); ?>/marketing-de-performance" class="btn-banner">Marketing de Performance</a>
         </div>
-        <div class="form-box">
-           
+        <div class="form-content">
+            <h2>Vamos Conversar?</h2>
+            <form id="form">
+                <div class="form-box row">
+                    <label>
+                        <span>Nome*</span>
+                        <input type="text" name="nome" placeholder="Seu Nome">
+                    </label>
+                    <label>
+                        <span>Whatsapp*</span>
+                        <input type="text" name="whatsapp" placeholder="(XX) x xxxx-xxxx">
+                    </label>
+                </div>
+                <div class="form-box row">
+                    <label>
+                        <span>E-mail*</span>
+                        <input type="email" name="email" placeholder="exemple@gmail.com">
+                    </label>
+                </div>
+                <div class="form-box row">
+                    <label>
+                        <span>Cargo*</span>
+                       <select name="cargo" id="">
+                        <option value="Cargo 1">Cargo 1</option> 
+                        <option value="Cargo 2">Cargo 2</option> 
+                       </select>
+                    </label>
+                </div>
+                <div class="form-box row">
+                    <label>
+                        <span>Segmento*</span>
+                       <select name="segmento" id="">
+                        <option value="Segmento 1">Segmento 1</option> 
+                        <option value="Segmento 2">Segmento 2</option> 
+                       </select>
+                    </label>
+                </div>
+                <div class="form-box row">
+                    <label>
+                        <span>Interesse*</span>
+                       <select name="interesse" id="">
+                        <option value="Interesse 1">Interesse 1</option> 
+                        <option value="Interesse 2">Interesse 2</option> 
+                       </select>
+                    </label>
+                </div>
+                <button type="submit">Faça um orçamento</button>
+            </form>
         </div>
     </div>
 </div>
