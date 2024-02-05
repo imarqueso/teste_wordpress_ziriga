5810bca7a757782903c3e4cdeb09064e681a1aa9
Igor Marques <soigormarques@gmail.com>
2024-02-05 03:04:19 -0300

    feat: Desenvolvimento seção cases

--- wp-content/themes/adsplay/page-template/midia-programatica.php
+++ wp-content/themes/adsplay/page-template/midia-programatica.php
@@ -275,6 +275,63 @@ Acesse diferentes inventários e agrupe eles sob as mesmas métricas, faça uplo
         </div>
     </div>
 </section>
+<section class="cases-titulo-container">
+    <div class="cases-titulo-content container">
+        <h3>Cases</h3><hr>
+    </div>
+</section>
+<section class="cases-container">
+    <div class="cases-content">
+        <a href="#" class="cases-card">
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/usaflex_banner.jpg" class="cases-card-banner">
+            <div class="cases-card-overlay">
+            </div>
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/usaflex_logo.png" class="cases-card-logo">
+        </a>
+        <a href="#" class="cases-card">
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/ifood_banner.jpg" class="cases-card-banner">
+            <div class="cases-card-overlay">
+           </div>
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/ifood_logo.png" class="cases-card-logo">
+        </a>
+        <a href="#" class="cases-card">
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/vale_banner.jpg" class="cases-card-banner">
+            <div class="cases-card-overlay">
+           </div>
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/vale_logo.png" class="cases-card-logo">
+        </a>
+        <a href="#" class="cases-card">
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/agroshow_banner.jpg" class="cases-card-banner">
+            <div class="cases-card-overlay">
+           </div>
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/agroshow_logo.png" class="cases-card-logo">
+        </a>
+        <a href="#" class="cases-card">
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/centernorte_banner.jpg" class="cases-card-banner">
+            <div class="cases-card-overlay">
+           </div>
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/centernorte_logo.png" class="cases-card-logo">
+        </a>
+        <a href="#" class="cases-card">
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/vale_banner.jpg" class="cases-card-banner">
+            <div class="cases-card-overlay">
+           </div>
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/vale_logo.png" class="cases-card-logo">
+        </a>
+        <a href="#" class="cases-card">
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/metrorio_banner.jpg" class="cases-card-banner">
+            <div class="cases-card-overlay">
+           </div>
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/metrorio_logo.png" class="cases-card-logo">
+        </a>
+        <a href="#" class="cases-card">
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/acer_banner.jpg" class="cases-card-banner">
+            <div class="cases-card-overlay">
+           </div>
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/adsplay/assets/images/acer_logo.png" class="cases-card-logo">
+        </a>
+    </div>
+</section>
 <?php if (have_posts()) :
     while (have_posts()) : the_post();
         the_content();
